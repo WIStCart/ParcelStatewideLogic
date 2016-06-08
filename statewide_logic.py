7f7449cc31a0eb01aedaa3437e077837d8496687
cjscheele <scheele.cjs@gmail.com>
2016-06-08 12:52:50 -0500

    Chris_Update
    Almost working...

--- statewide_logic.py
+++ statewide_logic.py
@@ -2,6 +2,7 @@ import arcpy
 from arcpy import env
 import re
 import csv
+import os
 
 #0 Pre-Process
 # cs test comment
@@ -18,10 +19,12 @@ double_field_list = ["CNTASSDVALUE_DBL","LNDVALUE_DBL","IMPVALUE_DBL","FORESTVAL
 
 #Create copy of feature class in memory
 arcpy.AddMessage("WRITING TO MEMORY")
-output_fc_temp = in_fc + "_WORKING"
+output_fc_temp = os.path.join("in_memory", "WORKING")
+auxClassTable = os.path.join(outDir,"unusualAuxClassTable")
+arcpy.AddMessage(output_fc_temp)
 arcpy.Delete_management("in_memory")
 dynamic_workspace = "in_memory"
-arcpy.FeatureClassToFeatureClass_conversion(in_fc,dynamic_workspace,output_fc_temp)
+arcpy.FeatureClassToFeatureClass_conversion(in_fc,dynamic_workspace,"WORKING")
 
 #Add double fields for processing
 arcpy.AddMessage("ADDING FIELDS")
@@ -38,19 +41,18 @@ arcpy.AddField_management(output_fc_temp,"GISACRES_DBL", "DOUBLE")
 arcpy.AddField_management(output_fc_temp,"NUM_CAST_FLAG", "TEXT", "", "", 50)
 
 #Parse SchoolDist csv to create two lists
-reader = csv.reader(open('school_district_codes.csv', 'r'))
+reader = csv.reader(open(os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])),'school_district_codes.csv'), 'r'))
 schoolDist_nameNo_dict = {}
 schoolDist_noName_dict = {}
 for row in reader:
    k, v = row
    schoolDist_noName_dict[k] = v
    schoolDist_nameNo_dict[v] = k
-
 #Create a table for the unusual AUXCLASS
 arcpy.AddMessage("CREATING AUXCLASS TABLE")
 arcpy.CreateTable_management(outDir,"unusualAuxClassTable")
-arcpy.AddField_management("unusualAuxClassTable","STATEID", "TEXT", "", "", 100)
-arcpy.AddField_management("unusualAuxClassTable","UNAUXCLASS", "TEXT", "", "", 150)
+arcpy.AddField_management(auxClassTable,"STATEID", "TEXT", "", "", 100)
+arcpy.AddField_management(auxClassTable,"UNAUXCLASS", "TEXT", "", "", 150)
 
 #1 Row operations
 
@@ -58,7 +60,10 @@ arcpy.AddField_management("unusualAuxClassTable","UNAUXCLASS", "TEXT", "", "", 1
 
 #State ID
 def calcStateid(row,cursor):
-	if row.getValue("PARCELID") == None:
+	arcpy.AddMessage(row)
+	parcelid = row.getValue("PARCELID")
+	arcpy.AddMessage(parcelid)
+	if parcelid == None:
 	    row.setValue("STATEID", row.getValue("PARCELFIPS"))
 	else:
 		calculated = row.getValue("PARCELFIPS") + row.getValue("PARCELID")
@@ -95,12 +100,13 @@ def calcImproved(row,cursor):
 def numValCast(row,cursor,field_list):
 	regexp = re.compile("[^0-9.]")
 	for field in field_list:
-		if "e" in row.getValue(field) or "E" in row.getValue(field):
-			row.setValue(field + "_DBL", float(row.getValue(field)))
-		elif regexp.search(word) is not None:
-			row.setValue("NUM_CAST_FLAG",field)
-		else:
-			row.setValue(field + "_DBL", float(row.getValue(field)))
+		if row.getValue(field) is not None:
+			if "e" in row.getValue(field) or "E" in row.getValue(field):
+				row.setValue(field + "_DBL", float(row.getValue(field)))
+			elif regexp.search(word) is not None:
+				row.setValue("NUM_CAST_FLAG",field)
+			else:
+				row.setValue(field + "_DBL", float(row.getValue(field)))
 
 #Unusual AUXCLASS: 
 def unusualAuxClass(row,cursor):
@@ -112,46 +118,44 @@ def unusualAuxClass(row,cursor):
 	for aClass in auxClassList:
 		if not any(aClass in s for s in auxClassDef):
 			unusualAuxClass.append(aClass)
-	if unusualAuxClass.len > 0:
+	if len(unusualAuxClass) > 0:
 		return ",".join(unusualAuxClass)
 	else:
 		return None
 
 arcpy.AddMessage("PROCESSING ROWS")
 updateCursor = arcpy.UpdateCursor(output_fc_temp)
-insertCursor = arcpy.InsertCursor("unusualAuxClassTable")
+
 for row in updateCursor:
 	rowCount += 1
-	calcStateid(row, updateCursor)
-	processSchoolDist(row,updateCursor,schoolDist_nameNo_dict,schoolDist_noName_dict)
-	calcImproved(row, updateCursor)
-	numValCast(row, updateCursor,double_field_list)
-
+	#calcStateid(row, updateCursor)
+	#processSchoolDist(row,updateCursor,schoolDist_nameNo_dict,schoolDist_noName_dict)
+	#calcImproved(row, updateCursor)
+	#numValCast(row, updateCursor,double_field_list)
+	stateid = row.getValue("STATEID")
 	#Unusual AUXCLASS
 	if row.getValue("AUXCLASS") is not None:
 		auxClass = unusualAuxClass(row,updateCursor)
 		if auxClass is not None:
+			insertCursor = arcpy.InsertCursor(auxClassTable)
 			inRow = insertCursor.newRow()
-    		inRow.setValue("STATEID",row.getValue("STATEID"))
+			inRow.setValue("STATEID","stateid")
     		inRow.setValue("UNAUXCLASS",auxClass)
     		insertCursor.insertRow(inRow)
-
-
+    		del(insertCursor)
 	if (rowCount % logEveryN) == 0:
 		arcpy.AddMessage("PROCESSED "+str(rowCount)+" RECORDS")
 del(updateCursor)
-del(inRow)
-del(insertCursor)	
+	
 
 #2 Column operations
-
+arcpy.AddMessage("PROCESSING COLUMNS")
 #Run summaries on certain fields
 def createSummarytables(output_fc_temp,outDir,outName):
 	fieldList = ["PREFIX","STREETNAME","STREETTYPE","SUFFIX"]
 	for i in fieldList:
 		arcpy.Frequency_analysis(output_fc_temp,outDir +"/"+outName+i+"_Summary",i)
 		
-<<<<<<< HEAD
 #Case and trim		
 def cleanCaseTrim(field,nullList,output_fc_temp):
 	query = '("' + field + '" IS NOT ' + "" + "NULL" + ')'
@@ -173,9 +177,6 @@ def cleanCaseTrim(field,nullList,output_fc_temp):
 			else:
 				count += 1
 					
-
-createSummarytables(output_fc_temp,outDir,outName)
-
 #null array 
 nullArray = ["<Null>", "<NULL>", "NULL", ""]
 
@@ -186,20 +187,13 @@ for field in fieldList:
         if field.type == "String":
             cleanCaseTrim(field.name,nullArray,output_fc_temp);
 
-#3 Misc operations (could be in separate script)
-
-#ROW/Hydro Standardization
-
-
-#4 Post-Process
+#3 Post-Process
 
 #Field map 
 #Run a merge into the template schema
 #Clear workspace
-=======
-createSummarytables(in_fc,outDir,outName)
+#createSummarytables(in_fc,outDir,outName)
 
 #3 Post-Process
 #Return a feature class
-arcpy.FeatureClassToFeatureClass_conversion(output_fc_temp, out_dir, outName)
->>>>>>> origin/master
+#arcpy.FeatureClassToFeatureClass_conversion(output_fc_temp, outDir, outName)
